cf92caaa83be0d0bbcbe46eb8fe8d4a2b4d957f6
Gabriel Sousa <gabriel.sousa@unimedvr.com.br>
2022-09-22 15:03:18 -0300

    Preparando Template

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
     <custom-navbar></custom-navbar>
-    <p class="text-center">{{ msg }}</p>
-    <custom-footer></custom-footer>
+    <div class="m-5 d-flex justify-content-center align-items-center">
+      <h3>{{ msg }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
 import { bus } from "./main";
-
+import { http } from "./main";
 export default {
   components: {},
   data() {
     return {
       msg: "Welcome to Your Vue.js + Vite App",
     };
+  },
+  mounted() {
+    http
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then(({ data }) => {
+        console.log(data);
+      });
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import axios from "axios";
 import navbar from "./components/header.vue";
-import footer from "./components/footer.vue";
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.component("custom-navbar", navbar);
-Vue.component("custom-footer", footer);
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
+// Config axios
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+export const http = axios;
 export const bus = new Vue();
 new Vue({
   el: "#app",
