65e6b4e0c93cec538387193ec996e002ee973857
Gabriel Sousa <gabriel.sousa@unimedvr.com.br>
2022-09-22 15:05:58 -0300

    Ajustes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import axios from "axios";
 import navbar from "./components/header.vue";
+
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
+//Custom Navbar
 Vue.component("custom-navbar", navbar);
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
+
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 
@@ -17,6 +22,8 @@ axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 export const http = axios;
+
+// Config Vue
 export const bus = new Vue();
 new Vue({
   el: "#app",
